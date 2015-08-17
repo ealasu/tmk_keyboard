f87abd4a7fa82e4dcda7de43d3e4df332e9afa14
eman <e@m.an>
2015-08-17 13:46:08 -0700

    add a fourth layer with vi keys

--- keyboard/atreus/keymap_eman.c
+++ keyboard/atreus/keymap_eman.c
@@ -14,26 +14,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_PGDN, KC_4, KC_5, KC_6, SHIFT(KC_EQUAL),   \
          SHIFT(KC_5), SHIFT(KC_6), KC_LBRC, KC_RBRC, SHIFT(KC_GRAVE), \
          SHIFT(KC_7), KC_1, KC_2, KC_3, KC_BSLS,       \
-         KC_FN1, SHIFT(KC_INS), KC_LGUI, KC_FN4, KC_BSPC, KC_LCTL, KC_LALT, \
+         KC_FN1, KC_FN5, KC_LGUI, KC_FN4, KC_BSPC, KC_LCTL, KC_LALT, \
          KC_SPC, KC_FN0, KC_DOT, KC_0, KC_EQUAL),
   /* 2: arrows and function keys */
   KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP,     KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
          KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11, \
          KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_FN3, KC_F1, KC_F2, KC_F3, KC_F12, \
-         KC_NO, KC_LSFT, KC_LGUI, KC_FN4, KC_BSPC,    KC_LCTL, KC_LALT,   KC_SPC, KC_FN2, KC_NO, KC_NO, KC_ENT)
+         KC_NO, KC_LSFT, KC_LGUI, KC_FN4, KC_BSPC,    KC_LCTL, KC_LALT,   KC_SPC, KC_FN2, KC_NO, KC_NO, KC_ENT),
+  /* 3: vi keys */
+  KEYMAP(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO, \
+         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+         KC_NO, KC_FN5, KC_LGUI, KC_FN4, KC_BSPC,    KC_LCTL, KC_LALT,   KC_SPC, KC_FN2, KC_NO, KC_NO, KC_ENT),
 };
 
-  const uint16_t PROGMEM fn_actions[] = {
+const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
   [1] = ACTION_LAYER_ON(2, 1),  // switch to layer 2
   [2] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
   [3] = ACTION_FUNCTION(BOOTLOADER),
-  [4] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
-  };
+  [4] = ACTION_LAYER_MOMENTARY(2),  // to second Fn overlay (function keys)
+  [5] = ACTION_LAYER_MOMENTARY(3),  // to third Fn overlay (vi keys)
+};
 
-  void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
-  {
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
   if (id == BOOTLOADER) {
-  bootloader();
+    bootloader();
   }
-  }
+}
